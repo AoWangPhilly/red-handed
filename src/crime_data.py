d95a6b07fd3e8e46a77d1445f12ca75628e8a2af
AoWangDrexel <aw3338@drexel.edu>
2020-07-05 02:00:18 -0400

    Add crime data class

---
+++ src/crime_data.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+
+class CrimeData():
+    def __init__(self, file="data/incidents2020.csv"):
+        self.file = file
+        self.df = pd.read_csv(file)
+
+    def __str__(self):
+        idx = self.getFile().find("20")
+        year = self.getFile()[idx:idx+4]
+        string = "Philadelphia Crime Data: {}\n{}".format(
+            year, self.getDataFrame().head())
+        return string
+
+    def getFile(self):
+        return self.file
+
+    def setFile(self, file):
+        self.file = file
+
+    def getDataFrame(self):
+        return self.df
+
+
+if __name__ == "__main__":
+    print(CrimeData())
