d79dbe10f413ba679f784da9967511c0a82d748b
AoWangDrexel <aw3338@drexel.edu>
2020-07-10 23:01:34 -0400

    Test markers and click event messages

--- src/js/map.js
+++ src/js/map.js
@@ -6,8 +6,27 @@ function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {
             lat: 39.9509,
-            lng: -75.1575
+            lng: -75.1875
         },
         zoom: 14
+    });
+
+    let marker = crimeMarker(39.96233372, -75.16144594);
+    google.maps.event.addListener(marker, "click", () => {
+        infowindow("<strong>Testing message</strong><br>Yes").open(map, marker);
+    });
+}
+
+const crimeMarker = (lat, lng) => {
+    return new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map,
+        title: "Test point"
+    });
+}
+
+const infowindow = (content) => {
+    return new google.maps.InfoWindow({
+        content: content
     });
 }
