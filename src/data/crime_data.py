c811a6ed896d9ec93151fcb0710b6f54a41bd40c
AoWangDrexel <aw3338@drexel.edu>
2020-07-12 19:17:04 -0400

    Clean and wrangle data from 2006-2020 to become geoJSON

--- src/data/crime_data.py
+++ src/data/crime_data.py
@@ -1,14 +1,31 @@
+"""
+description: crime_data.py
+author: ao wang
+date: 07/12/2020
+"""
+
 import pandas as pd
 import numpy as np
 from geojson import GeometryCollection, Point
+import json
 
 
 class CrimeData():
+    """
+    The class CrimeData is able to count the number of each crime, clean the data, 
+    format as GeoJSON, and save as it.
+
+    Attributes
+    ==========
+    file : str
+           The crime CSV filename
+    """
     def __init__(self, file="incidents2020.csv"):
         self.file = file
         self.df = pd.read_csv(file)
 
     def __str__(self):
+        """Prints out the file name and shows the beginning of the dataframe"""
         idx = self.getFile().find("20")
         year = self.getFile()[idx:idx+4]
         string = "Philadelphia Crime Data: {}\n{}".format(
@@ -16,9 +33,15 @@ class CrimeData():
         return string
 
     def clean(self):
+        # Drops any missing crime data
         cleaned = self.df.dropna()
+
+        # Drops duplicates
         if np.any(cleaned.duplicated):
             cleaned = cleaned.drop_duplicates()
+        
+        # Orders the crimes by earliest in the year
+        cleaned = cleaned.sort_values(by=["dispatch_date_time"])
         return cleaned
 
     def eachCrime(self):
@@ -36,8 +59,9 @@ class CrimeData():
     def convertGeoJson(self):
         cleanDf = self.clean()
         geoList = []
-        locations = df[['lat', 'lng']]
+        locations = cleanDf[['lat', 'lng']]
         locationList = locations.values.tolist()
+
         for point in range(len(locationList)):
             crimePoint = Point(locationList[point], properties={
                                "location_block": cleanDf.location_block.iloc[point],
@@ -47,8 +71,14 @@ class CrimeData():
         return GeometryCollection(geoList)
 
     def saveAsGeoJSON(self):
-        pass
+        idx = self.getFile().find("20")
+        year = self.getFile()[idx:idx+4]
+        fileName = "{}.json".format(year)
+
+        with open(fileName, "w") as crimeFile:
+            json.dump(self.convertGeoJson(), crimeFile)
 
 
 if __name__ == "__main__":
-    print(CrimeData().convertGeoJson())
+    def createFileName(year): return "incidents{}.csv".format(year)
+    [CrimeData(createFileName(year)).saveAsGeoJSON() for year in range(2015,2021)]
