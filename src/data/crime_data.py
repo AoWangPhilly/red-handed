bd1b72e105203ad125e373cd26b8d1e1c5b25908
AoWangDrexel <aw3338@drexel.edu>
2020-07-12 18:26:38 -0400

    virtual environment

--- src/data/crime_data.py
+++ src/data/crime_data.py
@@ -1,9 +1,10 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+from geojson import GeometryCollection, Point
 
 
 class CrimeData():
-    def __init__(self, file="data/incidents2020.csv"):
+    def __init__(self, file="incidents2020.csv"):
         self.file = file
         self.df = pd.read_csv(file)
 
@@ -15,7 +16,10 @@ class CrimeData():
         return string
 
     def clean(self):
-        pass
+        cleaned = self.df.dropna()
+        if np.any(cleaned.duplicated):
+            cleaned = cleaned.drop_duplicates()
+        return cleaned
 
     def eachCrime(self):
         return self.getDataFrame().text_general_code.value_counts()
@@ -29,6 +33,22 @@ class CrimeData():
     def getDataFrame(self):
         return self.df
 
+    def convertGeoJson(self):
+        cleanDf = self.clean()
+        geoList = []
+        locations = df[['lat', 'lng']]
+        locationList = locations.values.tolist()
+        for point in range(len(locationList)):
+            crimePoint = Point(locationList[point], properties={
+                               "location_block": cleanDf.location_block.iloc[point],
+                               "text_general_code": cleanDf.text_general_code.iloc[point],
+                               "dispatch_date_time": cleanDf.dispatch_date_time.iloc[point]})
+            geoList.append(crimePoint)
+        return GeometryCollection(geoList)
+
+    def saveAsGeoJSON(self):
+        pass
+
 
 if __name__ == "__main__":
-    print(CrimeData().eachCrime())
+    print(CrimeData().convertGeoJson())
