0a14e2cc3ce474802cf08869df6c61602e99d326
AoWangDrexel <aw3338@drexel.edu>
2020-07-13 23:39:44 -0400

    Add features to copy and save the crime of specific day

--- src/js/table.js
+++ src/js/table.js
@@ -1,9 +1,12 @@
+// https://metadata.phila.gov/#home/datasetdetails/5543868920583086178c4f8e/representationdetails/570e7621c03327dc14f4b68d/
 d3.csv("src/data/cleanedincidents2020.csv").then(data => {
     let monthData = [];
-    for (let crime of data) {
+    for (const [index, crime] of data.entries()) {
         let date = new Date(crime.dispatch_date_time);
         if (date.getMonth() == 0 && date.getDay() == 3) {
-            monthData.push([crime.dc_dist,
+            monthData.push([
+                index,
+                crime.dc_dist,
                 crime.psa,
                 crime.dispatch_date_time,
                 crime.location_block,
@@ -15,11 +18,16 @@ d3.csv("src/data/cleanedincidents2020.csv").then(data => {
             break;
         }
     }
-    console.log(monthData);
     $(document).ready(() => {
         $('#datatable').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ],
             data: monthData,
             columns: [{
+                    title: "#"
+                }, {
                     title: "dc_dist"
                 },
                 {
@@ -40,7 +48,7 @@ d3.csv("src/data/cleanedincidents2020.csv").then(data => {
                 {
                     title: "lng"
                 }
-            ]
+            ],
         });
     });
 
