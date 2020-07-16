6e482e70b5f15e1f7943c2cee541a3e1d3bda691
AoWangDrexel <aw3338@drexel.edu>
2020-07-16 11:59:03 -0400

    Add in more details to area chart

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -9,6 +9,13 @@ d3.json("src/data/dashboard/crimePerYear.json").then(crime => {
             labels: Object.keys(crime),
             datasets: [{
                 label: 'Philadelphia Crime (2006-2020)',
+                lineTension: 0.3,
+                pointRadius: 5,
+                pointHoverRadius: 5,
+                pointBorderWidth: 2,
+                pointHitRadius: 20,
+                pointBorderColor: "rgba(255,255,255,0.8)",
+                pointBackgroundColor:'rgb(255, 99, 132)',
                 backgroundColor: 'rgb(255, 99, 132, 0.2)',
                 borderColor: 'rgb(255, 99, 132)',
                 data: Object.values(crime)
