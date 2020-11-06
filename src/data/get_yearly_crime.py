17e7cd11c2ebb00a18ddf748d7a507f5945c15e2
AoWangDrexel <aw3338@drexel.edu>
2020-11-05 21:26:53 -0500

    Add in convert_str_to_dt

--- src/data/get_yearly_crime.py
+++ src/data/get_yearly_crime.py
@@ -1,36 +1,64 @@
 import pandas as pd
 import numpy as np
+import datetime
 import json
 from sodapy import Socrata
 
 
 def create_schema():
+    """Creates a schema that describes all the dataset's columns
+
+    Returns:
+        pd.core.DataFrame: The dataset schema
+    """
+
     crime_schema = pd.read_csv(
         'NYPD_Incident_Level_Data_Column_Descriptions.csv', index_col='Column')
     return crime_schema
 
 
 def get_user_cred():
+    """Accesses my app token and login info for the API
+
+    Returns:
+        list of str: list of token, username, and password
+    """
     with open('app_token') as f:
         return f.read().split('\n')
 
 
-def seperate_by_yr(df):
+def separate_by_yr(df):
+    """Separates the full dataset into a dictionary holding year-assigned crimes
+
+    Args:
+        df (pd.core.DataFrame): Raw dataframe with mixed dates
+
+    Returns:
+        dictionary of dataframes: the crimes assigned by year
+    """
     year_dict = {}
 
     # Grab only the year and make it an int type
     out_df = df.copy(deep=True)
-    out_df['Year'] = out_df['CMPLNT_FR_DT'].str[6:].fillna(0)
-    out_df['Year'] = out_df['Year'].astype('int16')
+    out_df['Year'] = out_df['CMPLNT_TO_DT'].dt.year
 
     # Get only 2006-2019
     latest_yr, earliest_yr = 2006, 2020
-    out_df = out_df[out_df['Year'] > latest_yr]
+    out_df = out_df[(out_df['Year'] > latest_yr) &
+                    (out_df['Year'] < earliest_yr)]
     for yr in range(latest_yr, earliest_yr):
         select_yr = out_df[out_df['Year'] == yr]
         year_dict[yr] = select_yr
 
     return year_dict
+
+
+def convert_str_to_dt(df):
+    out_df = df.copy(deep=True)
+    out_df[['CMPLNT_TO_DT', 'CMPLNT_FR_DT', 'RPT_DT']] = \
+        out_df[['CMPLNT_TO_DT', 'CMPLNT_FR_DT', 'RPT_DT']].apply(
+            pd.to_datetime, format='%Y-%m-%d', errors='coerce')
+    return out_df
 
 
 if __name__ == '__main__':
@@ -47,8 +75,12 @@ if __name__ == '__main__':
     # results = client.get_all("qgea-i56i")
     results = client.get('qgea-i56i')  # default 1000 rows
     crime_df_yrs = pd.DataFrame.from_records(results)
-    crime_df_yrs.set_index('CMPLNT_NUM', inplace=True)
+    crime_df_yrs.columns = crime_df_yrs.columns.str.upper()
 
-    sep_year_crime = seperate_by_yr(crime_df_yrs)
+    crime_df_yrs.set_index('CMPLNT_NUM', inplace=True)
+    
+    crime_df_yrs = convert_str_to_dt(crime_df_yrs)
+
+    sep_year_crime = separate_by_yr(crime_df_yrs)
     for yr in range(2007, 2020):
         sep_year_crime[yr].to_csv('years/{}.csv'.format(yr))
