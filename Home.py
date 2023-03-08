2f5bcdf6db5d2725682598fbf32d7dbc54c18ddc
AoWangPhilly <aw3338@drexel.edu>
2023-03-07 21:18:06 -0500

    test out streamlit

--- Home.py
+++ Home.py
@@ -1,154 +1,54 @@
-import streamlit as st
-import pandas as pd
 from os.path import join
-from typing import Dict
+from typing import Tuple
+
+import pandas as pd
 import plotly.express as px
-import statsmodels.api as sm
-import plotly.graph_objects as go
-import numpy as np
-import leafmap.foliumap as leafmap
-import streamlit.components.v1 as components
-
-# http://michael-harmon.com/blog/crimetime.html
-st.set_page_config(layout="wide")
-
-START_YEAR = 2006
-END_YEAR = 2021
-
-
-def rename_values(df: pd.DataFrame) -> pd.DataFrame:
-    df.replace({
-        'HARRASSMENT 2': 'HARASSMENT',
-        'ESCAPE 3': 'ESCAPE',
-        'ASSAULT 3 & RELATED OFFENSES': 'ASSAULT & RELATED OFFENSES',
-        'CRIMINAL MISCHIEF & RELATED OF': 'CRIMINAL MISCHIEF',
-        'OFF. AGNST PUB ORD SENSBLTY &': 'OFFENSES AGAINST PUBLIC ORDER/ADMINISTRATION',
-        'OTHER STATE LAWS (NON PENAL LA': 'OTHER STATE LAWS (NON PENAL LAW)',
-        'ENDAN WELFARE INCOMP': 'ENDANGERING WELFARE OF INCOMPETENT',
-        'AGRICULTURE & MRKTS LAW-UNCLASSIFIED': 'AGRICULTURE & MARKETS LAW',
-        'DISRUPTION OF A RELIGIOUS SERV': 'DISRUPTION OF A RELIGIOUS SERVICE',
-        'LOITERING/GAMBLING (CARDS, DIC': 'GAMBLING',
-        'OFFENSES AGAINST MARRIAGE UNCL': 'OFFENSES AGAINST MARRIAGE',
-        'HOMICIDE-NEGLIGENT,UNCLASSIFIE': 'HOMICIDE-NEGLIGENT',
-        'E': 'UNKNOWN',
-        'D': 'BUSINESS/ORGANIZATION',
-        'F': 'FEMALE',
-        'M': 'MALE'
-    }, inplace=True)
-    return df
+import streamlit as st
+from pyspark import SparkConf
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame as PySparkDataFrame
+from pyspark.sql.functions import (
+    col,
+    year,
+    month,
+    dayofmonth,
+    hour
+)
 
 
-def get_crime_by_year(year: int) -> pd.DataFrame:
-    df = pd.read_parquet(
-        join("data", f"NYPD_Complaint_Data_Historic_{year}.parquet"),
-        engine="fastparquet"
-    ).iloc[:10_000]
-    return rename_values(df)
+@st.cache_resource
+def initializeSpark() -> Tuple[SparkSession, SparkContext]:
+    """Create a Spark Session for Streamlit app"""
+    conf = SparkConf().setAppName("crime-processor").setMaster("local")
+    spark = SparkSession.builder.config(conf=conf).getOrCreate()
+    return spark, spark.sparkContext
 
 
-def get_all_crimes() -> Dict[int, pd.DataFrame]:
-    output = {year: get_crime_by_year(year) for year in range(START_YEAR, END_YEAR + 1)}
-    return output
+@st.cache_data
+def getCrimesPerMonth(_sdf: PySparkDataFrame) -> pd.DataFrame:
+    crimeTimes = _sdf.select(
+        year("CMPLNT_FR").alias("CMPLNT_FR_YEAR"),
+        month("CMPLNT_FR").alias("CMPLNT_FR_MONTH"),
+        dayofmonth("CMPLNT_FR").alias("CMPLNT_FR_DAY"),
+        hour("CMPLNT_FR").alias("CMPLNT_FR_HOUR")
+    ).cache()
+
+    crimesPerMonth = crimeTimes.groupBy(["CMPLNT_FR_YEAR", "CMPLNT_FR_MONTH"]).count().sort(
+        [col("CMPLNT_FR_YEAR"), col("CMPLNT_FR_MONTH")]
+    ).toPandas()
+
+    crimesPerMonth["Date"] = crimesPerMonth.CMPLNT_FR_MONTH.map(str) + "/" + crimesPerMonth.CMPLNT_FR_YEAR.map(str)
+    crimesPerMonth.Date = pd.to_datetime(crimesPerMonth.Date)
+    crimesPerMonth.set_index("Date", inplace=True)
+    crimesPerMonth.drop(columns=["CMPLNT_FR_YEAR", "CMPLNT_FR_MONTH"], inplace=True)
+    return crimesPerMonth
 
 
-crimes = get_all_crimes()
+spark, _ = initializeSpark()
+processedSDF = spark.read.load(path=join("data", "NYPD_Complaint_Data_Historic.parquet"), format="parquet")
 
+crimesPerMonth = getCrimesPerMonth(processedSDF)
 
-def get_top_n_crimes(df: pd.DataFrame, nth: int = 10):
-    data = df.OFNS_DESC.value_counts().iloc[:nth].sort_values(ascending=False)
-    fig = px.bar(data)
-    fig.update_layout(
-        title=f"Top {nth} Crimes in {df.CMPLNT_FR.iloc[0].year}",
-        xaxis_title="Crime Type",
-        yaxis_title="Number of Occurrences",
-    )
-    fig.update_traces(
-        hovertemplate="<i>Crime</i>: %{x}"
-                      "<br><i>Occurrences</i>: %{y}<br>"
-                      "<extra></extra>",
-    )
-    fig.update_layout(showlegend=False)
-    return fig
-
-
-def show_number_of_crimes_per_year(crimes: Dict[int, pd.DataFrame]) -> pd.DataFrame:
-    crimes_per_year = pd.DataFrame(pd.Series({year: len(df) for year, df in crimes.items()}, name="Count"))
-    fig = go.Figure()
-
-    line = go.Scatter(
-        x=crimes_per_year.index,
-        y=crimes_per_year.Count,
-        hovertemplate="<i>Year</i>: %{x}"
-                      "<br><i>Total Crime</i>: %{y}<br>"
-                      "<extra></extra>",
-        mode='lines',
-        name="Actual"
-    )
-    fig.add_trace(line)
-    fig.update_layout(
-        title="Total Crime Count per Year",
-        xaxis_title="Year",
-        yaxis_title="Count",
-    )
-    fig.update_layout(showlegend=False)
-
-    X = sm.add_constant(crimes_per_year.index)
-    model = sm.OLS(crimes_per_year.Count, X).fit()
-    intercept, slope = model.params
-
-    future_x = np.array(range(START_YEAR, END_YEAR + 2))
-    fig.add_trace(
-        go.Scatter(
-            x=future_x,
-            y=intercept + slope * future_x,
-            hovertemplate="<i>Year</i>: %{x}"
-                          "<br><i>Predicted Total Crime</i>: %{y}<br>"
-                          "<extra></extra>",
-            name="Predicted"
-
-        )
-    )
-    fig.update_layout(legend_title_text='Trend', showlegend=True)
-    return fig
-
-
-st.write("# RedHanded :wave:")
-
-with st.container():
-    number_of_crimes_per_year_line = show_number_of_crimes_per_year(crimes=crimes)
-
-    st.plotly_chart(number_of_crimes_per_year_line, use_container_width=True)
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-
-        year = st.selectbox(
-            label="Select a year to analyze:",
-            options=range(START_YEAR, END_YEAR + 1),
-            index=END_YEAR - START_YEAR
-        )
-        crime_year = get_crime_by_year(year=year)
-        top_crimes = get_top_n_crimes(crime_year)
-
-        st.plotly_chart(top_crimes, use_container_width=True)
-
-    with col2:
-        st.plotly_chart(px.pie(crime_year, names="LAW_CAT_CD", title="Level of Offense"), use_container_width=True)
-
-# https://huggingface.co/spaces/giswqs/Streamlit
-with st.echo():
-    m = leafmap.Map(center=[40.730610, -73.935242], zoom=10)
-    m.add_geojson("Borough Boundaries.geojson", layer_name='NY Bouroughs')
-    print(crime_year[["Latitude", "Longitude"]])
-    m.add_points_from_xy(
-        crime_year,
-        x="Latitude",
-        y="Longitude",
-    )
-    m.to_streamlit()
-
-# https://data.cityofnewyork.us/Public-Safety/Police-Precincts/78dh-3ptz
-# st.write(crime_2021)
-# crime_2021.rename(columns={"Latitude": "LAT", "Longitude": "LON"}, inplace=True)
-# st.map(crime_2021)
+monthly = px.line(crimesPerMonth, x=crimesPerMonth.index, y="count")
+monthly
