619f5cbfa1093f702daec30c546feefba6a2eddd
AoWangPhilly <aw3338@drexel.edu>
2023-03-11 22:52:40 -0500

    update about with readme

--- pages/4_About.py
+++ pages/4_About.py
@@ -1,28 +1,6 @@
 import streamlit as st
 
-st.write(
-    """# RedHanded
+with open("README.md", "r") as f:
+    readme = f.read()
 
-## DSCI 521 Final Project
-
-### Author: Ao Wang
-
-### Setup (MacOS M1)
-
-1. `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
-2. `brew install openjdk@11`
-3. `brew install apache-spark`
-4. `python3 -m venv venv`
-5. `source venv/bin/activate`
-6. `pip3 install -r requirements.txt`
-
-### How to Run
-
-After all required software and packages are installed, run `streamlit run Home.py` to run the Streamlit web application. There's no special setup to run the Jupyter Notebooks.
-
-### Challenges
-
-### Limitations
-
-    """
-)
+st.write("\n".join(readme.split("\n")[1:]))
